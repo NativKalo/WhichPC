bdb0f3c05e4fa6da72207bf805f839a2dbd255af
NativKalo <60568537+NativKalo@users.noreply.github.com>
2021-06-10 15:40:16 +0300

    pc

--- PuppetPhones.js
+++ PuppetPhones.js
@@ -3,14 +3,13 @@ const Crawler = require("crawler");
 let connection = require("./connection-wrapper");
 
 async function puppetCameraScores(phones) {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
     for (let i = 0; i < phones.length; i++) {
         let key_words = phones[i];
         const link = 'https://www.phonearena.com/';
 
-        const browser = await puppeteer.launch({ headless: true, slowMo: 100, devtools: true });
-
         try {
-            const page = await browser.newPage();
 
             await page.setDefaultNavigationTimeout(0);
 
@@ -26,16 +25,9 @@ async function puppetCameraScores(phones) {
 
             await page.keyboard.press('Enter');
 
-            await page.waitFor(3000);
-
             await page.waitForSelector('.widget-tilePhoneCard a')
 
-            const url = await getHref(
-                page,
-                `.widget-tilePhoneCard a`
-            );
-
-            // await page.goto(url, { waitUntil: 'domcontentloaded' });
+            const url = await page.evaluate((key_words) => { return document.querySelector(`img[alt="${key_words}"]`).parentElement.parentElement.getAttribute('href') }, key_words)
 
             var c = new Crawler({
                 maxConnections: 10,
@@ -48,21 +40,31 @@ async function puppetCameraScores(phones) {
                             console.log(key_words)
                             insertCameraScoreToDB(key_words, res.$(elem).html())
                         });
+                        res.$('.widgetSpecs > *:nth-child(5) tr:nth-child(1) td').each(function (i, elem) {
+                            // console.log(res.$(elem).html())
+                            // console.log(key_words)
+                            insertBatteryScoreToDB(key_words, res.$(elem).html())
+                        });
+                        for (i of res.$('.widgetSpecs > *:nth-child(7) tbody > tr td').html()) {
+
+                            console.log(i)
+
+                        }
+                        // res.$('.widgetSpecs > *:nth-child(7) tr:nth-child(10) td').each(function (i, elem) {
+                        //     insertSelfieCameraScoreToDB(key_words, res.$(elem).html())
+                        // });
                     }
                     done();
                 }
             });
             c.queue(url);
-
-            await page.close();
-
-            await browser.close();
-
         } catch (error) {
             console.log(error);
-            await browser.close();
         }
     }
+    await page.close();
+
+    await browser.close();
 }
 
 const getHref = (page, selector) =>
@@ -72,7 +74,19 @@ const getHref = (page, selector) =>
     );
 
 const insertCameraScoreToDB = async (phone, cameraScore) => {
-    let sql = `INSERT INTO camspecs (phone_name,phone_camera_score)  values(?,?)`;
+    let sql = `INSERT INTO rearcamspecs (phone_name,phone_camera_score)  values(?,?)`;
+    let parameters = [phone, cameraScore];
+    insertPhone = await connection.executeWithParameters(sql, parameters);
+}
+
+const insertBatteryScoreToDB = async (phone, batteryscore) => {
+    let sql = `INSERT INTO batteryspecs (phone_name,phone_battery_capacity)  values(?,?)`;
+    let parameters = [phone, batteryscore];
+    insertPhone = await connection.executeWithParameters(sql, parameters);
+}
+
+const insertSelfieCameraScoreToDB = async (phone, cameraScore) => {
+    let sql = `INSERT INTO selfiecamspecs (phone_name,phone_camera_score)  values(?,?)`;
     let parameters = [phone, cameraScore];
     insertPhone = await connection.executeWithParameters(sql, parameters);
 }
